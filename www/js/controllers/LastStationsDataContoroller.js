ec3101cbbc544f4671433c0091fbdc0dd298a274
elvinoza <elvinoza@gmail.com>
2016-05-12 21:48:38 +0300

    show full weather summary in modal

--- www/js/controllers/LastStationsDataContoroller.js
+++ www/js/controllers/LastStationsDataContoroller.js
@@ -1,6 +1,8 @@
-app.controller('LastStationsDataCtrl', function ($auth, $state, $scope, $rootScope, $stateParams, ApiService, ionicMaterialMotion) {
+app.controller('LastStationsDataCtrl', function ($auth, $state, $scope, $rootScope, $stateParams, ApiService, ionicMaterialMotion, $ionicModal, $timeout) {
 
     var vm = this;
+
+    $scope.datas = [];
 
     vm.getLastStationsData = function(){
         ApiService.getUserLastStationsData($rootScope.currentUser.id).success(function(data) {
@@ -16,6 +18,21 @@ app.controller('LastStationsDataCtrl', function ($auth, $state, $scope, $rootSco
         setTimeout(function() {
             ionicMaterialMotion.blinds();
         }, 500);
+    };
+
+    $ionicModal.fromTemplateUrl('my-modal.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function(modal) {
+        $scope.modal = modal;
+    });
+
+    $scope.openModal = function(index) {
+        $scope.modalData = $scope.datas[index];
+        $scope.modal.show();
+        $timeout(function () {
+            $scope.modal.hide();
+        }, 4000);
     };
 
     $scope.blinds();
